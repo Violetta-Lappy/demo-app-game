83e7ebd9433a7d9432b3dacfef4594b1fd53b8d0
hoanglongdummy <104409899+hoanglongplanner@users.noreply.github.com>
2023-09-27 18:58:24 +0700

    Update Stuffs

---
+++ applecatch-raylib-cpp-20230927/applecatch-raylib-cpp-20230927/internal/ProgramRender.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "ProgramConfig.h"
+
+class ProgramRender {
+private:
+	ProgramConfig m_config;
+public:
+	ProgramRender();
+	~ProgramRender();
+public:
+	ProgramConfig GetConfig() const {
+		return m_config;
+	}
+	void SetConfig(ProgramConfig arg_config) {
+		m_config = arg_config;
+	}
+public:
+	void Start();
+	void Update(float arg_dt, float arg_unscaledDt);
+	void Terminate();
+};
